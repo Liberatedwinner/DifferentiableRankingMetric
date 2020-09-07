35991d2f4163e2b0914889954d80b7034bdd0b2e
liberatedwinner <kale.lbnr@gmail.com>
2020-09-07 14:39:11 +0900

    삭제함:        backup.sh         삭제함:        run-cml.sh 	삭제함:        run-cml0.sh 	삭제함:        run-ours.sh 	삭제함:        run-ours0.sh 	삭제함:        run-wmf.sh

--- run-wmf.sh
+++
@@ -1,6 +0,0 @@
-python implicit-pt.py --dataset_name=ml-20m-712 --model_name=wmf
-python implicit-pt.py --dataset_name=epinion-712 --model_name=wmf 
-python implicit-pt.py --dataset_name=sk-712 --model_name=wmf
-python implicit-pt.py --dataset_name=melon-712 --model_name=wmf
-python implicit-pt.py --dataset_name=pinterest-712 --model_name=wmf 
-python implicit-pt.py --dataset_name=ciao-712 --model_name=wmf
